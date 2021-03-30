2373895d10cf2c5a0bd05026b4eebc058f9ce891
azuchi <azuchi@chaintope.com>
2021-03-30 11:19:15 +0900

    Add curve constants

--- lib/bls.rb
+++ lib/bls.rb
@@ -4,4 +4,37 @@ require 'bls/version'
 
 module BLS
   class Error < StandardError; end
+
+  module Curve
+
+    # characteristic
+    P = 0x1a0111ea397fe69a4b1ba7b6434bacd764774b84f38512bf6730d2a0f6b0f6241eabfffeb153ffffb9feffffffffaaab
+    # order
+    R = 0x73eda753299d7d483339d80809a1d80553bda402fffe5bfeffffffff00000001
+    # cofactor
+    H = 0x396c8c005555e1568c00aaab0000aaab
+    # Generator's coordinates
+    G_X = 0x17f1d3a73197d7942695638c4fa9ac0fc3688c4f9774b905a14e3a3f171bac586c55e83ff97a1aeffb3af00adb22c6bb
+    G_Y = 0x08b3f481e3aaa0f1a09e30ed741d8ae4fcf5e095d5d00af600db18cb2c04b3edd03cc744a2888ae40caa232946c5e7e1
+    B = 4
+
+    # G2
+    P2 = 0x1a0111ea397fe69a4b1ba7b6434bacd764774b84f38512bf6730d2a0f6b0f6241eabfffeb153ffffb9feffffffffaaab ** 2 - 1
+    H2 = 0x5d543a95414e7f1091d50792876a202cd91de4547085abaa68a205b2e5a7ddfa628f1cb4d9e82ef21537e293a6691ae1616ec6e786f0c70cf1c38e31c7238e5
+    G2_X = [
+      0x024aa2b2f08f0a91260805272dc51051c6e47ad4fa403b02b4510b647ae3d1770bac0326a805bbefd48056c8c121bdb8,
+      0x13e02b6052719f607dacd3a088274f65596bd0d09920b61ab5da61bbdc7f5049334cf11213945d57e5ac7d055d042b7e
+    ].freeze
+    G2_Y = [
+      0x0ce5d527727d6e118cc9cdc6da2e351aadfd9baa8cbdd3a76d429a695160d12c923ac9cc3baca289e193548608b82801,
+      0x0606c4a02ea734cc32acd2b02bc28b99cb3e287e85a763af267492ab572e99ab3f370d275cec1da1aaa9075ff05f79be
+    ].freeze
+    B2 = [4, 4].freeze
+
+    # The BLS parameter x for BLS12-381
+    X = 0xd201000000010000
+    H_EFF = 0xbc69f08f2ee75b3584c6a0ea91b352888e2a8e9145ad7689986ff031508ffe1329c2f178731db956d82bf015d1212b02ec0ec69d7477c1ae954cbc06689f6a359894c0adebbf6b4e8020005aaa95551
+
+  end
+
 end
