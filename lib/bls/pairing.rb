e99e35d20b5b455fda8b80460e3681649e60e7ff
azuchi <azuchi@chaintope.com>
2022-09-30 17:49:19 +0900

    Add arguments validation

--- lib/bls/pairing.rb
+++ lib/bls/pairing.rb
@@ -9,6 +9,8 @@ module BLS
   # @return [BLS::Fq12]
   # @return [BLS::PairingError] Occur when p.zero? or q.zero?
   def pairing(p, q, with_final_exp: true)
+    raise ArgumentError, 'p should be BLS::PointG1 object' unless p.is_a?(BLS::PointG1)
+    raise ArgumentError, 'q should be BLS::PointG2 object' unless p.is_a?(BLS::PointG2)
     raise PairingError, 'No pairings at point of Infinity' if p.zero? || q.zero?
 
     p.validate!
